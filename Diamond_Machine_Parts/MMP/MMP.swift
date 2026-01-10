a6209de906ed1ce04fc8a674693e167dab643d48
Philip Turner <philipturner.AR@gmail.com>
2026-01-10 13:26:55 -0500

    Still working on optimizing this parser

--- Diamond_Machine_Parts/MMP/MMP.swift
+++ Diamond_Machine_Parts/MMP/MMP.swift
@@ -8,18 +8,67 @@ struct MMP {
     // Parsing the string:
     // - Each space separates a word
     // - Omit parentheses and commas
-    let lines = string.split(separator: "\n").map(String.init)
-    for line in lines {
-      for character in line.utf8 {
-        
-      }
-//      let rawWords = line.split(separator: " ")//.map(String.init)
+    let lines = string.split(separator: "\n")
+    for var line in lines {
       var newWords: [String] = []
       
+      line.withUTF8 {
+        var pendingWord: [UInt8] = []
+        for character in $0 {
+          if character == 0x20 {
+            newWords.append(String(
+              decoding: pendingWord, as: UTF8.self))
+            pendingWord = []
+            continue
+          }
+          
+          if character == 0x28 {
+            continue
+          }
+          if character == 0x29 {
+            continue
+          }
+          if character == 0x2C {
+            continue
+          }
+          pendingWord.append(character)
+        }
+        if pendingWord.count > 0 {
+          newWords.append(String(
+            decoding: pendingWord, as: UTF8.self))
+        }
+      }
+      
+//      for character in line.utf8 {
+//        if character == 0x20 {
+//          newWords.append(String(
+//            decoding: pendingWord, as: UTF8.self))
+//          pendingWord = []
+//          continue
+//        }
+//        
+//        if character == 0x28 {
+//          continue
+//        }
+//        if character == 0x29 {
+//          continue
+//        }
+//        if character == 0x2C {
+//          continue
+//        }
+//        pendingWord.append(character)
+//      }
+//      if pendingWord.count > 0 {
+//        newWords.append(String(
+//          decoding: pendingWord, as: UTF8.self))
+//      }
+      
+//      let rawWords = line.split(separator: " ").map(String.init)
+//      var newWords: [String] = []
+//
 //      for rawWord in rawWords {
 //        var newCharacters: [UInt8] = []
 //        for character in rawWord.utf8 {
-//          break
 //          if character == 0x28 {
 //            continue
 //          }
@@ -30,7 +79,6 @@ struct MMP {
 //            continue
 //          }
 //          newCharacters.append(character)
-//          break
 //        }
 //        
 //        let newWord = String(decoding: newCharacters, as: UTF8.self)
@@ -40,21 +88,21 @@ struct MMP {
         continue
       }
       
-      if newWords[0] == "csys" && false {
+      if newWords[0] == "csys" {
         let subsequence = Array(newWords[1...])
         let namedView = NamedView(words: subsequence)
         namedViews[namedView.name] = namedView
       }
       
-      if newWords[0] == "atom" && false {
-//        let subsequence = Array(newWords[2...])
-//        let atom = Self.createAtom(words: subsequence)
-        let atom = SIMD4<Float>(
-          Float(newWords[3])!,
-          Float(newWords[4])!,
-          Float(newWords[5])!,
-          Float(newWords[2])!
-        )
+      if newWords[0] == "atom" {
+        let subsequence = Array(newWords[2...])
+        let atom = Self.createAtom(words: subsequence)
+//        let atom = SIMD4<Float>(
+//          Float(newWords[3])!,
+//          Float(newWords[4])!,
+//          Float(newWords[5])!,
+//          Float(newWords[2])!
+//        )
         
         // WARNING: Serialized MMP files are 1-indexed.
         guard let atomID = UInt32(newWords[1]) else {
@@ -66,7 +114,7 @@ struct MMP {
         topology.atoms.append(atom)
       }
       
-      if newWords[0] == "bond1" && false {
+      if newWords[0] == "bond1" {
         let subsequence = Array(newWords[1...])
         for word in subsequence {
           // Using 1-index notation.
