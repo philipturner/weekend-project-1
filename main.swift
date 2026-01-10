c260c5476eb87eb5077ecf10d152ba39cd8f9303
Philip Turner <philipturner.AR@gmail.com>
2026-01-10 14:39:07 -0500

    Save progress

--- main.swift
+++ main.swift
@@ -7,9 +7,8 @@ import xTB
 
 var path = FileManager.default.currentDirectoryPath
 path += "/Sources/Workspace/Diamond_Machine_Parts/Blocks/"
-path += "hex_clip_pin.mmp"
+path += "1x1_end_beam.mmp"
 
-let start1 = Date()
 let fileData = FileManager.default.contents(atPath: path)
 guard let fileData else {
   fatalError("Could not get file at path: \(path)")
@@ -18,34 +17,75 @@ let fileString = String(data: fileData, encoding: .utf8)
 guard let fileString else {
   fatalError("Could not create string.")
 }
-let end1 = Date()
 
-let start2 = Date()
 let mmp = MMP(string: fileString)
-let end2 = Date()
-
-let start3 = Date()
 mmp.validate()
-let end3 = Date()
-
-@MainActor
-func profile(start: Date, end: Date) {
-  let timeInterval = end.timeIntervalSince(start)
-
-  var microsecondsPerAtom = timeInterval / 1e-6
-  microsecondsPerAtom /= Double(mmp.topology.atoms.count)
-  
-  print("profiling data")
-  print("- latency:", Float(timeInterval / 1e-3), "ms")
-  print("- throughput:", Float(microsecondsPerAtom), "μs/atom")
-}
 
 print()
 print("byte count:", fileData.count / 1000, "KB")
 print("atom count:", mmp.topology.atoms.count)
 print("bond count:", mmp.topology.bonds.count)
 
-print()
-profile(start: start1, end: end1)
-profile(start: start2, end: end2)
-profile(start: start3, end: end3)
+// MARK: - Launch Application
+
+@MainActor
+func createApplication() -> Application {
+  // Set up the device.
+  var deviceDesc = DeviceDescriptor()
+  deviceDesc.deviceID = Device.fastestDeviceID
+  let device = Device(descriptor: deviceDesc)
+  
+  // Set up the display.
+  var displayDesc = DisplayDescriptor()
+  displayDesc.device = device
+  displayDesc.frameBufferSize = SIMD2<Int>(1620, 1620)
+  displayDesc.monitorID = device.fastestMonitorID
+  let display = Display(descriptor: displayDesc)
+  
+  // Set up the application.
+  var applicationDesc = ApplicationDescriptor()
+  applicationDesc.device = device
+  applicationDesc.display = display
+  applicationDesc.upscaleFactor = 3
+  
+  applicationDesc.addressSpaceSize = 4_000_000
+  applicationDesc.voxelAllocationSize = 500_000_000
+  applicationDesc.worldDimension = 64
+  let application = Application(descriptor: applicationDesc)
+  
+  return application
+}
+let application = createApplication()
+
+for atomID in mmp.topology.atoms.indices {
+  let atom = mmp.topology.atoms[atomID]
+  application.atoms[atomID] = atom
+}
+
+@MainActor
+func modifyCamera() {
+  let namedView = mmp.namedViews["LastView"]
+  guard let namedView else {
+    fatalError("Could not retrieve named view.")
+  }
+  
+  let rotation = Quaternion<Float>(
+    angle: Float.pi / 180 * -90,
+    axis: SIMD3(1, 0, 0)
+  )
+  
+  application.camera.basis.0 = rotation.act(on: SIMD3(1, 0, 0))
+  application.camera.basis.1 = rotation.act(on: SIMD3(0, 1, 0))
+  application.camera.basis.2 = rotation.act(on: SIMD3(0, 0, 1))
+  application.camera.fovAngleVertical = Float.pi / 180 * 60
+  
+  application.camera.position = SIMD3(-2, 10, 0)
+}
+
+application.run {
+  modifyCamera()
+  
+  var image = application.render()
+  image = application.upscale(image: image)
+  application.present(image: image)
+}
